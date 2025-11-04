772af78899dfd21d4bb23125b57245aaa93fef1c
Alexandre <johndoe@example.com>
2025-11-04 17:54:37 +0100

    d

---
+++ formulaire.php
@@ -0,0 +1,21 @@
+<?php include('entete.php');?>
+    <header>
+        <h1>TP4 Connexion</h1>
+    </header>
+   <body>
+        <h1>Connexion</h1>
+        
+        <form action="connexion.php">
+            <label>Identifiant : </label>
+            <input type="text" name="identifiant" required>
+            <br>
+            <br>
+            <label>Mot de passe : </label>
+            <input type="password" name="motdepasse" required>
+            <br>
+            <br>
+            <button type="submite">Se connecter</button>
+        </form>
+
+    </body>
+ <?php include('pieddepage.php');?>
--- index.php
+++ index.php
@@ -3,20 +3,9 @@
         <h1>TP4 Connexion</h1>
     </header>
    <body>
-        <h1>Connexion</h1>
-        
-        <form action="connexion.php">
-            <label>Identifiant : </label>
-            <input type="text" name="identifiant" required>
-            <br>
-            <br>
-            <label>Mot de passe : </label>
-            <input type="password" name="motdepasse" required>
-            <br>
-            <br>
-            <button type="submite">Se connecter</button>
-        </form>
-
+        <h1>Bienvenue</h1>
+        <br>
+        <h2>Connectez vous<h2>
     </body>
  <?php include('pieddepage.php');?>
 
