c47b0d2007949937816abc12f1ec21ec96ea0188
Alexandre <johndoe@example.com>
2025-11-04 15:21:10 +0100

    minimal mmi

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" prefix="og: https://ogp.me/ns#">
+<head>
+  <meta charset="utf-8" />
+  <title>TP4</title>
+</head>
+<body>
+    <header>
+        <!-- en-tête -->
+            <h1>Document minimal en MMI</h1>
+        <nav>
+        <!-- menu principal -->
+        </nav>
+     </header>        
+        
+        <!-- partie principale -->
+        <section>
+            <h2>Titre de section</h2>
+         </section>
+        
+        <footer>
+        <!-- pied de page -->
+            <p>Copyright © Mathieu Mangeot</p>
+        </footer>
+</body>
+</html>
