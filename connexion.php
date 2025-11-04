98c9374ab5e255e8ee6ef6e6bf319841e6be9228
Alexandre <johndoe@example.com>
2025-11-04 17:36:10 +0100

    h2

--- connexion.php
+++ connexion.php
@@ -3,8 +3,10 @@
         <h1>TP4 Connexion</h1>
     </header>
    <body>
+    <h2>
         <?php $identifiant = $_REQUEST["identifiant"]; 
         echo "Bonjour ", $identifiant; ?>
+    <h2>
 
 
  <?php include('pieddepage.php');?>
