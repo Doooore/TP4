a6412b36227231546c9d0202c6cc1c494ecb47f0
Alexandre <johndoe@example.com>
2025-11-04 16:11:10 +0100

    creation connexion/bouton

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
 <html lang="fr" prefix="og: https://ogp.me/ns#">
-<head>
-  <meta charset="utf-8" />
-  <title>TP4</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header id="entete">
-            <h1>Document minimal en MMI</h1>
-     </header>        
+    <head>
+        <meta charset="utf-8" />
+        <title>TP4</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header id="entete">
+           <h1>Document minimal en MMI</h1>
+        </header>        
+
+        <h1>Connexion</h1>
+        
+        <form action="connexion.php">
+            <label>Identifiant:</label>
+            <imput type="text" name="identifiant" required>
+
+            <label>Mot de passe:</label>
+            <imput type="password" name="motdepasse" required>
+
+            <button type="submite">Se connecter </button>
+        </form>
+            
 
         <section id="Titre">
             <h2>Titre de section</h2>
          </section>
         
         <footer>
-
             <p>Copyright © Mathieu Mangeot</p>
         </footer>
-</body>
+    </body>
 </html>
